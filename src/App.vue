85d51ff362216047c5eefed66200d840374de2a8
emily <emilyvicki601@gmail.com>
2024-06-26 23:29:05 +0200

    style: ui adjust

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
   <div id="app" class="overflow-auto">
     <AppHeader class="w-full h-16 px-32 md:px-4" />
-    <router-view class="home-bg" />
-    <portal-target
-      name="modal"
-      multiple
-    />
+    <div class="flex w-10/12 m-auto space-x-2">
+      <router-view :selectedCategory="selectedCategory" class="home-bg" />
+    </div>
+    <portal-target name="modal" multiple />
   </div>
 </template>
 
@@ -15,6 +14,17 @@ import AppHeader from "@/components/AppHeader";
 export default {
   name: "App",
   components: { AppHeader },
+  data() {
+    return {
+      selectedCategory: null,
+    };
+  },
+  methods: {
+    // handleCategorySelected(category) {
+    //   console.log("Selected Category:", category); // Debugging log
+    //   this.selectedCategory = category;
+    // },
+  },
 };
 </script>
 
