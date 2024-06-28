338e429e42563de0fc66266d5ec7f5934bfb5469
emily <emilyvicki601@gmail.com>
2024-06-28 11:53:44 +0200

    chore: add mock data

--- src/mockData.js
+++ src/mockData.js
@@ -1,10 +1,11 @@
 import Mock from "mockjs";
+const brands = ["LENOVO", "ACER", "微星", "華碩", "雷蛇", "DELL", "技嘉", "威剛", "三星"];
 
 const mockData = Mock.mock({
   "categories|1-5": [
     {
       id: "@increment",
-      name: "@title(3, 5)",
+      name: "@title(1, 3)",
       icon: "@image('24x24', '#000', '#fff', 'icon')",
       limit: 4,
       "items|4-56": [
@@ -14,18 +15,25 @@ const mockData = Mock.mock({
           "imgSrc|1": ["src/assets/cool-1.jpeg", "src/assets/item-6.jpeg"],
           price: "@integer(100, 30000)",
           isHotItem: "@boolean",
-          尺寸: "@sentence(1, 5)",
-          CPU: "@sentence(1, 2)",
-          RAM: "@sentence(1, 3)",
-          SSD: "@sentence(1, 2)",
-          VGA: "@sentence(1, 4)",
-          LAN: "@sentence(1, 3)",
-          周邊: "@sentence(1, 2)",
-          OS: "@sentence(1, 3)",
+          size: "@sentence(1, 5)",
+          cpu: "@sentence(1, 2)",
+          ram: "@sentence(1, 3)",
+          ssd: "@sentence(1, 2)",
+          vga: "@sentence(1, 4)",
+          lan: "@sentence(1, 3)",
+          other: "@string(1, 2)",
+          os: "@sentence(1, 3)",
+          brand: "@pick(brands)"
         },
       ],
     },
   ],
+});
+
+Mock.Random.extend({
+  brand: function() {
+    return this.pick(brands);
+  }
 });
 
 export default mockData.categories;
