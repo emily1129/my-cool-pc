ed57d9381a046566687828e2d308d41a21c7dd6a
emily <emilyvicki601@gmail.com>
2024-07-04 16:17:36 +0200

    feat: function for add cart button

--- src/views/Item/ItemDetail.vue
+++ src/views/Item/ItemDetail.vue
@@ -2,7 +2,9 @@
   <div
     class="flex md:flex-col justify-center items-center w-full mx-auto my-5 p-8 md:px-5 md:pb-12 space-y-2 bg-white dark:bg-slate-700 rounded-md"
   >
-    <div class="flex items-center justify-center w-1/2 md:w-full h-full dark:text-white">
+    <div
+      class="flex items-center justify-center w-1/2 md:w-full h-full dark:text-white"
+    >
       <img
         src="https://dlcdnwebimgs.asus.com/gain/ac709e89-8fca-4cf5-b63b-f0426714078b/w185/fwebp"
         alt="item-img"
@@ -41,7 +43,7 @@
         class="w-full h-14 flex md:flex-col justify-between space-x-4 md:space-x-0"
       >
         <div
-          class="w-1/2 md:w-full px-6 py-3 md:py-1 flex items-center justify-center border border-gray-300 rounded space-x-7 text-xl mb-2 dark:bg-slate-300"
+          class="w-1/2 md:w-full px-6 py-3 md:py-1 flex items-center justify-center border border-gray-300 space-x-7 text-xl mb-2 dark:bg-slate-300"
         >
           <button
             class="text-gray-600 hover:text-gray-800 focus:outline-none"
@@ -62,7 +64,8 @@
           </button>
         </div>
         <button
-          class="w-1/2 md:w-full h-12 flex items-center justify-center space-x-2 px-6 md:py-2 font-semibold text-white bg-orange-500 rounded-lg hover:bg-orange-600 focus:outline-none hover:scale-105 transition duration-500 ease-in-out"
+          class="w-1/2 md:w-full h-12 flex items-center justify-center space-x-2 px-6 md:py-2 font-semibold text-white bg-orange-500 hover:bg-orange-600 focus:outline-none hover:scale-105 transition duration-500 ease-in-out"
+          @click="addToVuexCart"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -78,9 +81,33 @@
               d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
             />
           </svg>
-          <p class="text-md">Add to cart</p>
+          <span class="text-md">Add to cart</span>
         </button>
       </div>
+    </div>
+    <div
+      v-if="showMessage"
+      :class="{'opacity-100': showMessage, 'opacity-0': !showMessage}"
+      role="alert"
+      class="fixed top-20 left-1/2 transform -translate-x-1/2 flex w-3/4 px-4 py-4 text-base bg-green-200 shrink-0 transition ease-in-out duration-300"
+    >
+      <div class="shrink-0">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+          ></path>
+        </svg>
+      </div>
+      <div class="ml-3 mr-12 text-green-700">The item was succesfully added to you shopping cart.</div>
     </div>
   </div>
 </template>
@@ -88,6 +115,7 @@
 <script>
 import mockData from "@/mockData";
 import ItemSpec from "./components/ItemSpec.vue";
+import { mapActions } from "vuex";
 
 export default {
   name: "ItemDetail",
@@ -97,12 +125,32 @@ export default {
     return {
       item: {},
       quantity: 1,
+      showMessage: false
     };
   },
   created() {
     this.fetchItemDetail();
   },
+  computed: {
+    currentItemId() {
+      const currentId = this.$route.params.itemId
+      return currentId;
+    }
+  },
+  watch: {
+    currentItemId: {
+      immediate: true,
+      handler(newVal) {
+        this.fetchItemDetail(newVal);
+      }
+    }
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.fetchItemDetail(this.currentId);
+    next();
+  },
   methods: {
+    ...mapActions(["addToCart"]),
     fetchItemDetail() {
       for (const category of mockData) {
         const item = category.items.find((i) => i.id === Number(this.itemId));
@@ -118,6 +166,16 @@ export default {
       if (this.quantity > 1) {
         this.quantity--;
       }
+    },
+    addToVuexCart() {
+      this.addToCart({ ...this.item, quantity: this.quantity });
+      this.showSuccessMessage();
+    },
+    showSuccessMessage() {
+      this.showMessage = true;
+      setTimeout(() => {
+        this.showMessage = false;
+      }, 3000); 
     },
   },
 };
